7d903668e7c8f1d9109784eb13152db7ca51576e
bugra <farukbugrademirel@gmail.com>
2021-05-29 12:31:19 +0200

    Fix sorting of aggregation

--- src/routers/onbodydevice.js
+++ src/routers/onbodydevice.js
@@ -43,33 +43,92 @@ router.get(endPoint + '/allmicroneedles/:id', auth, async (req, res) => {
 
     try {
         const device = await OnBodyDevice.findById(req.params.id)
+
         if(!device) {
             return res.status(404).send('No device is found by provided id: ' + req.params.id )
-        }
+        } else {
+            if (req.query.interval === 'seconds' || req.query.interval === undefined) {
+                await device.populate({
+                    path: 'microneedles',
+                    populate: {
+                        path: 'measurements',
+                        model: 'Measurement',
+                        select: 'time value -owner -_id',
+                        options: {
+                            limit: 600,
+                            sort : { time: -1 },
+                        },
 
-        await device.populate({
-            path: 'microneedles',
-            populate: {
-                path: 'measurements',
-                model: 'Measurement',
-                select: 'time value -owner -_id',
-                options: {
-                    limit: 600,
-                    sort : { time: -1 },
-                },
+                    }
+                }).execPopulate()
 
+                const updatedMicroneedlesToBeSendBack = []
+
+                for (let i = 0; i < device.microneedles.length; i++) {
+                    let objForm = device.microneedles[i].toObject()
+                    objForm["measurements"] = device.microneedles[i].measurements
+                    updatedMicroneedlesToBeSendBack.push(objForm)
+                }
+                return res.status(200).send(updatedMicroneedlesToBeSendBack)
+
+            } else if (req.query.interval === 'minutes') {
+
+                await device.populate('microneedles').execPopulate()
+                const updatedMicroneedlesToBeSendBack = []
+
+                for (let i = 0; i < device.microneedles.length; i++) {
+                    const agg = await Measurement.aggregate([
+                        {
+                            $match: { owner: device.microneedles[i]._id }
+                        },
+                        {
+                            $sort: {time: 1}
+                        },
+                        {
+                            $project: {
+                                _id: "$_id",
+                                value: "$value",
+                                time : {
+                                    $dateFromString: {
+                                        dateString: {
+                                            $dateToString: {
+                                                date: {
+                                                    $toDate: {
+                                                        $multiply: ["$time", 1000]
+                                                    }
+                                                }, format: "%Y-%m-%dT%H:%M"
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            $group: {
+                                _id: "$time",
+                                value: { $first:"$value"}
+                            }
+                        },
+                        { "$sort": {"_id": 1}},
+                        {"$limit": 600}
+                    ])
+
+                    let objForm = device.microneedles[i].toObject()
+                    objForm.measurements = []
+                    agg.forEach( m => {
+                        const entry = {
+                            time: new Date(m._id).getTime() / 1000,
+                            value: m.value
+                        }
+                        objForm.measurements.push(entry)
+                    })
+                    updatedMicroneedlesToBeSendBack.push(objForm)
+                }
+                return res.status(200).send(updatedMicroneedlesToBeSendBack)
+            } else {
+                return res.status(400).send('Bad URL request')
             }
-        }).execPopulate()
-
-        const updatedMicroneedlesToBeSendBack = []
-
-        for (let i = 0; i < device.microneedles.length; i++) {
-            let objForm = device.microneedles[i].toObject()
-            objForm["measurements"] = device.microneedles[i].measurements
-            updatedMicroneedlesToBeSendBack.push(objForm)
         }
-
-        res.status(200).send(updatedMicroneedlesToBeSendBack)
     } catch (e) {
         res.status(500).send(e.message)
     }
@@ -154,8 +213,9 @@ router.delete(endPoint + '/:id', auth, async (req, res) => {
         if (!deletedDevice) {
             res.status(404).send('Device is not found by id: ' + req.params.id)
         } else {
-            const deleted = await Microneedle.deleteMany({ owner: deletedDevice._id })
-            for (const mns of deleted) {
+            const toBeDeleted = await Microneedle.find({owner: deletedDevice._id})
+            await Microneedle.deleteMany({ owner: deletedDevice._id })
+            for (const mns of toBeDeleted) {
                 await Measurement.deleteMany( { owner: mns._id })
             }
             res.status(200).send(deletedDevice)
